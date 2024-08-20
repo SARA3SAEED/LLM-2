de714a1be03625d3727a0bc5aed97099b9dffee0
Sara almutairi <146086114+SARA3SAEED@users.noreply.github.com>
2024-08-20 16:34:16 +0300

    Add files via upload

---
+++ neural_searcher.py
@@ -0,0 +1,40 @@
+from qdrant_client import QdrantClient
+from sentence_transformers import SentenceTransformer
+from qdrant_client.models import Filter
+    
+city_of_interest = "Berlin"
+# Define a filter for cities
+city_filter = Filter(**{
+    "must": [{
+        "key": "city", # Store city information in a field of the same name 
+        "match": { # This condition checks if payload field has the requested value
+            "value": city_of_interest
+        }
+    }]
+})
+
+class NeuralSearcher:
+    def __init__(self, collection_name):
+        self.collection_name = collection_name
+        # Initialize encoder model
+        self.model = SentenceTransformer("all-MiniLM-L6-v2", device="cpu")
+        # initialize Qdrant client
+        self.qdrant_client = QdrantClient("http://localhost:6333")
+    def search(self, text: str):
+        # Convert text query into vector
+        vector = self.model.encode(text).tolist()
+
+        # Use `vector` for search for closest vectors in the collection
+        search_result = self.qdrant_client.search(
+            collection_name=self.collection_name,
+            query_vector=vector,
+            #query_filter=None,  # If you don't want any filters for now
+            query_filter=city_filter,# TEST WITH AND WITHOUT FILTER
+            limit=5,  # 5 the most closest results is enough
+        )
+        # `search_result` contains found vector ids with similarity scores along with the stored payload
+        # In this function you are interested in payload only
+        payloads = [hit.payload for hit in search_result]
+        return payloads
+    
+        
